fe448d22fba6802de26b994d18950bb70d87a62a
AvivBuzaglo <avivbuzaglo99@gmail.com>
2025-08-19 21:48:28 +0300

    Add block hours to ChooseTime

--- frontend-react/src/cmps/ChooseTime.jsx
+++ frontend-react/src/cmps/ChooseTime.jsx
@@ -1,18 +1,61 @@
 import React, { useState, useEffect } from "react"
 import { orderService } from "../services/order/order.service.local.js"
+import { availableOrdersService } from '../services/order/availableOrder.service.local'
 
 export function ChooseTime({order, setOrder, setReadyToSave}) {
     const [orderToEdit, setOrderToEdit] = useState({...order})
+    const [blockedHours, setBlockedHours] = useState([])
 
     const times1 = ['9:00', '9:20', '9:40', '10:00', '10:20', '10:40', '11:00'] 
     const times2 = ['11:20', '11:40', '12:00', '12:20', '12:40', '13:00', '13:20']
     const times3 = ['13:40', '14:00', '14:20', '14:40', '15:00']
-    const times4 =['9:00', '9:20', '9:40', '10:00', '10:20', '10:40', '11:00', '11:20', '11:40', '12:00', '12:20', '12:40', '13:00', '13:20', '13:40', '14:00', '14:20', '14:40', '15:00']
+    const times4 = ['9:00', '9:20', '9:40', '10:00', '10:20', '10:40', '11:00', '11:20', '11:40', '12:00', '12:20', '12:40', '13:00', '13:20', '13:40', '14:00', '14:20', '14:40', '15:00', '15:10', '15:30', '15:50', '16:10', '16:30']
 
         useEffect(() => {
             setOrder(orderToEdit) 
         }, [orderToEdit])
 
+
+        useEffect(() => {
+            async function getBlocked() {
+                const result = await availableOrdersService.queryHours()
+                result.forEach(element => {
+                    element.date === orderToEdit.date ? setBlockedHours(element.hours) : setBlockedHours([])
+                })
+            }
+            getBlocked()
+
+            async function checkOrdered() {
+                const result = await orderService.query()
+                result.forEach(element => {
+                    element.date === orderToEdit.date ? setBlockedHours(prev => [...prev, element.start]) : ''
+                })
+            }
+            checkOrdered()
+
+            async function getHoursBetween() {
+                const result = await orderService.query()
+                let isMicro
+
+                result.forEach(element => {
+                    if(element.date === orderToEdit.date) {
+                        element.care === 'micro' ? isMicro = true : isMicro = false
+                        
+                        if(isMicro) {
+                            const index1 = times4.indexOf(element.start)
+                            const index2 = times4.indexOf(element.end)
+                            const occupiedHours = times4.slice(index1 + 1, index2)
+                            console.log(occupiedHours);
+                            
+                            setBlockedHours(prev => [...prev, ...occupiedHours])
+                        }
+                        
+                    }
+
+                })
+            }
+            getHoursBetween()
+        }, [])           
 
     const handleTimeClicked = (time) => {
         let endTime = setEndTime(time)
@@ -61,21 +104,21 @@ export function ChooseTime({order, setOrder, setReadyToSave}) {
                         <tr>
                             {times1.map((time) => (
                                 <td key={time}>
-                                    <button className="time-btn" onClick={() => handleTimeClicked(time)}>{time}</button>
+                                   { !(blockedHours.includes(time)) ? <button className="time-btn" onClick={() => handleTimeClicked(time)}>{time}</button> : ''}
                                 </td>
                             ))}
                         </tr>
                         <tr>
                             {times2.map((time) => (
                                 <td key={time}>
-                                    <button className="time-btn" onClick={() => handleTimeClicked(time)}>{time}</button>
+                                    { !(blockedHours.includes(time)) ? <button className="time-btn" onClick={() => handleTimeClicked(time)}>{time}</button> : ''}
                                 </td>
                             ))}                                
                         </tr>
                         <tr>
                             {times3.map((time) => (
                                 <td key={time}>
-                                    <button className="time-btn" onClick={() => handleTimeClicked(time)}>{time}</button>
+                                    { !(blockedHours.includes(time)) ? <button className="time-btn" onClick={() => handleTimeClicked(time)}>{time}</button> : ''}
                                 </td>
                             ))}                                
                         </tr>                            
