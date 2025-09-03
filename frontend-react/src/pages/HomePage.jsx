6f6efe9f4f9cfa0bcb08cf5391b4347da6ff8716
AvivBuzaglo <avivbuzaglo99@gmail.com>
2025-09-03 18:13:06 +0300

    Add Svgs cmp, Add contack links to HomePage

--- frontend-react/src/pages/HomePage.jsx
+++ frontend-react/src/pages/HomePage.jsx
@@ -8,6 +8,8 @@ import { NoticeBoard } from "../cmps/NoticeBoard"
 import { ContactInfo } from "../cmps/ContactInfo"
 import { UserLogout } from "../cmps/UserLogout"
 import { userService } from "../services/user/user.service.local"
+import { homePageSvgs } from "../cmps/Svgs"
+import { NavLink } from "react-router-dom"
 
 export function HomePage() {
     const user = useSelector(storeState => storeState.userModule.user)
@@ -47,6 +49,14 @@ export function HomePage() {
                     </div>
                 </section>
             )}
+            <div className="contact-links">
+                <span>ליצירת קשר</span> 
+                <div className="contact-icons">
+                    <a href="https://www.instagram.com/lin__eyebrows?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" rel="noopener noreferrer">{homePageSvgs.instagramLogo}</a>
+                    <a href="https://wa.me/972544692777" target="_blank" rel="noopener noreferrer">{homePageSvgs.whatsappLogo}</a>
+                    <a href="https://www.facebook.com/share/16ufNJDxXc/?mibextid=wwXIfr" target="_blank" rel="noopener noreferrer">{homePageSvgs.facebookLogo}</a>
+                </div>
+            </div>
         </section >
     )
 }
